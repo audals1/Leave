ab2e58fac624e74aee55b9d72227d423e95f5aed
명민 <audals3@gmail.com>
2023-07-17 21:24:48 +0900

    230717

--- Assets/Scripts/PressStart.cs
+++ Assets/Scripts/PressStart.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PressStart : MonoBehaviour
 {
@@ -18,7 +19,7 @@ public class PressStart : MonoBehaviour
             yield return new WaitForSeconds(0.3f);
             count++;
         }
-        
+        SceneManager.LoadScene("Game");
     }
     void PressAnykey()
     {
